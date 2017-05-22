8a9ba2c3cabd2b77a4cfecb6d94e3d462447cdf9
yyqyearn <yyqyearn@gmail.com>
2017-05-22 17:19:29 +0800

    no message

--- screens/New.js
+++ screens/New.js
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   Button,
   KeyboardAvoidingView,
+  Picker,
   TextInput
 } from "react-native";
 
@@ -17,15 +18,47 @@ class New extends Component {
       <Button title="保存" onPress={() => navigation.navigate("New")} />
     )
   });
+  state = {
+    behavior: "padding",
+    language: "java"
+  };
 
   render() {
     console.log(this.props.navigation.state.params.projects);
     return (
       <View style={styles.container}>
-        <Text>New</Text>
+        <KeyboardAvoidingView
+          style={styles.rowContainer}
+          behavior={this.state.behavior}
+        >
+          <Text>请选择APP:</Text>
+          <TextInput
+            placeholder="请选择"
+            style={styles.textInput}
+            onTouchStart={this.projectDidClick}
+          />
+        </KeyboardAvoidingView>
+        <KeyboardAvoidingView
+          style={styles.rowContainer}
+          behavior={this.state.behavior}
+        >
+          <Text>请输入版本:</Text>
+          <TextInput placeholder="TextInput" style={styles.textInput} />
+        </KeyboardAvoidingView>
+        <View style={styles.pickerContainer}>
+          <Picker
+            selectedValue={this.state.language}
+            onValueChange={lang => this.setState({ language: lang })}
+          >
+            <Picker.Item label="Java" value="java" />
+            <Picker.Item label="JavaScript" value="js" />
+          </Picker>
+        </View>
       </View>
     );
   }
+
+  projectDidClick() {}
 }
 
 // define your styles
@@ -33,8 +66,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#2c3e50"
+    alignItems: "center"
+    // backgroundColor: "#2c3e50"
+  },
+  textInput: {
+    borderRadius: 5,
+    borderWidth: 1,
+    height: 44,
+    width: 200,
+    paddingHorizontal: 10
+  },
+  pickerContainer: {},
+  rowContainer: {
+    padding: 20,
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
   }
 });
 
